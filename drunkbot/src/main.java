1664ea3fa5671b800287da2daac21205cac0d9eb
yarko3 <yarko3@hotmail.com>
2014-01-25 12:37:21 -0800

    adding src

---
+++ drunkbot/src/main.java
@@ -0,0 +1,19 @@
+// main file
+
+public class main {
+
+
+	public static void main(String[] args) {
+		
+		//greet user
+		
+		//get input
+		
+		//return output
+		
+		//ask again/exit
+		
+
+	}
+
+}
